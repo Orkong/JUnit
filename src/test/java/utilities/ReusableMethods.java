d165bddead16b18096a7187c9e5c6eebb94acbdb
John <orkun.e@hotmail.com>
2024-01-17 15:59:36 -0500

    9.gun

--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -1,9 +1,11 @@
 package utilities;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class ReusableMethods {
 
@@ -26,5 +28,25 @@ public class ReusableMethods {
             stringlist.add(eachElemenet.getText());
         }
         return  stringlist;
+    }
+
+
+    public static void titleIleWindowDegistir(String hedefTitle, WebDriver driver){
+
+        Set<String> whdSeti = driver.getWindowHandles();
+        System.out.println(whdSeti);
+
+        for (String eachWhd : whdSeti
+        ) {
+            driver.switchTo().window(eachWhd);
+
+            String oldugumuzSayfaTitle = driver.getTitle();
+
+            if (oldugumuzSayfaTitle.equals(hedefTitle)){
+                System.out.println(oldugumuzSayfaTitle);
+                break;
+            }
+        }
+
     }
 }
