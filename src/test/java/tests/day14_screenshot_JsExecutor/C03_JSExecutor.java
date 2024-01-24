45a1fcb9f07c71edfba981c29d7ab9c1f76ab26f
John <orkun.e@hotmail.com>
2024-01-24 14:16:59 -0500

    14.gun

---
+++ src/test/java/tests/day14_screenshot_JsExecutor/C03_JSExecutor.java
@@ -0,0 +1,41 @@
+package tests.day14_screenshot_JsExecutor;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import utilities.ReusableMethods;
+import utilities.TestBase;
+
+public class C03_JSExecutor extends TestBase {
+
+    @Test
+    public void test01(){
+
+        // Testotomasyonu anasayfaya gidin
+        driver.get("https://www.testotomasyonu.com");
+
+        // Alt kisimda bulunan "Best Sport Shoes" bolumune kadar asagi inin
+
+        JavascriptExecutor jse= (JavascriptExecutor) driver;
+
+
+
+        // basligin "Best Sport Shoes For Multi Gender." oldugunu test edin
+        WebElement bestSportElementi= driver.findElement(By.xpath("(//*[@class='blog-heading'])[2]"));
+
+        // jse ile istedigimiz bestspor elementine kadar assagi inelim
+        jse.executeScript("arguments[0].scrollIntoView();",bestSportElementi);
+        ReusableMethods.bekle(3);
+
+        String expectedBaslik="Best Sport Shoes For Multi Gender.";
+        String actualBaslik=bestSportElementi.getText();
+
+        ReusableMethods.webElementScreenshot(bestSportElementi,"best");
+        Assert.assertEquals(expectedBaslik,actualBaslik);
+
+
+
+    }
+}
